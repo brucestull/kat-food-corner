8de86c30c1d9cd6d8a821576683f434d2658e199
FlynntKnapp <bruce.stull@gmail.com>
2023-12-26 05:56:24 -0500

    Add html title tag to index view.

--- kat_food_corner/views.py
+++ kat_food_corner/views.py
@@ -3,8 +3,14 @@ from django.http import HttpResponse
 from kat_food_corner.settings import THE_SITE_NAME
 
 
+THE_APP_NAME = "KatFoodCorner"
+
+
 def index(request):
     """
     Simple function-based view to display the food list
     """
-    return HttpResponse(f"Hello, Foods! You're at the {THE_SITE_NAME} site.")
+    return HttpResponse(
+        f"<title>{THE_SITE_NAME} - {THE_APP_NAME}</title>"
+        f"Hello, Foods! You're at the {THE_SITE_NAME}:{THE_APP_NAME} site."
+    )
